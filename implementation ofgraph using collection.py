9426bfadc59cadc5d96acda4ee92445f5c787ce0
RamyaDeviTarra <113753025+RamyaDeviTarra@users.noreply.github.com>
2023-03-11 14:32:02 +0530

    Add files via upload

---
+++ implementation ofgraph using collection.py
@@ -0,0 +1,20 @@
+#implemantation of graph using collection of modules
+#import diction for graph
+from  collection import defaultdict
+graph = defaultdict(list)
+def addEdge(graph,u,v):
+    graph[u].append(v)
+def generate_edge(graph):
+    edges = []
+    for node in graph:
+        for neighbour in graph[node]:
+addEdge(graph,)
+addEdge(graph,)
+addEdge(graph,)
+addEdge(graph,)
+addEdge(graph,)
+addEdge(graph,)
+addEdge(graph,)
+addEdge(graph,)
+
+    
