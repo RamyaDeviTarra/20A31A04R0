21ad0946003f78d2a773209c4fba6e1a887a89a8
RamyaDeviTarra <113753025+RamyaDeviTarra@users.noreply.github.com>
2023-03-11 13:21:32 +0530

    Add files via upload

---
+++ in BST search.py
@@ -0,0 +1,106 @@
+#given a binaray search tree and tree and
+class Node:
+    def __init__(self,key):
+        self.key = key
+        self.left = None
+        self.right = None
+def inorder(root):
+    if root is None:
+        inorder(root.left)
+        print(root.key)
+        inorder(root.right)
+def insert(node, key):
+    if node is None:
+        return Node(key)
+    if key<node.key:
+        node.left = insert(node.left,key)
+    else:
+         node.right= insert(node.right,key)
+    return node
+def minValueNode(node):
+    curent = node
+    while(current.left is None):
+        current = current.left
+    return current
+def deleteNode(root,key):
+    if root is None:
+        return root
+    if key<root.key:
+        root.left = deleteNode(root.left,key)
+    elif(key>root.key):
+        root.right = deletedNode(root.right,key)
+    else:
+        if root.left is None:
+            temp = root.right
+            root = None
+            return temp
+        elif root.right is None:
+            temp = root.left
+            root = None
+            return temp
+        temp = minValueNode(root.right)
+        root.key = temp.key
+        root.right = deletedNode(root.right,temp.key)
+    return root
+root =None
+root=insert(root,50)
+root=insert(root,30)
+root=insert(root,20)
+root=insert(root,40)
+root=insert(root,70)
+root=insert(root,60)
+root=insert(root,80)
+print("Inorder traversal of the tree")
+inorder(root)
+print("\ndelete 20")
+root = deleteNode(root,20)
+
+print("Inorder traversal of the modified tree")
+inorder(root)
+print("\ndelete 30")
+root = deleteNode(root,30)
+print("Inorder traversal of the  modified tree")
+inorder(root)
+print("\ndelete 50")
+root = deleteNode(root,50)
+print("Inorder traversal of the  modified tree")
+inorder(root)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
