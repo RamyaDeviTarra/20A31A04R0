9776f46c3fd7fc8aff9e69fbb6bcacb0ea62406c
RamyaDeviTarra <113753025+RamyaDeviTarra@users.noreply.github.com>
2023-03-11 13:14:02 +0530

    Add files via upload

--- binary traversals.py
+++ binary traversals.py
@@ -8,9 +8,18 @@ def printInorder(root):
         #1st recursion on left child
         printInorder(root.left)
         #print the data of node
-        print(root.val, end = " " ),
+        print(root.value,end = ' ')
         #now recursion on right child
         printInorder(root.right)
+
+def printPreorder(root):
+    if root:
+        #print the data of node
+        print(root.value,end = ' ')
+        #1st recursion on left child
+        printPreorder(root.left)
+         #now recursion on right child
+        printPreorder(root.right)
 def printPostorder(root):
     if root:
         #1st recursion on left child
@@ -18,25 +27,18 @@ def printPostorder(root):
          #now recursion on right child
         printPostorder(root.right)
         #print the data of node
-        print(root.val, end = " " ),
-def printPreorder(root):
-    if root:
-        #print the data of node
-        print(root.val, end = " " ),
-        #1st recursion on left child
-        printPreorder(root.left)
-         #now recursion on right child
-        printPreorder(root.right)
+        print(root.value,end = ' ')
 root = Node(1)
 root.left = Node(2)
 root.right = Node(3)
 root.left.left = Node(4)
 root.left.right = Node(5)
-print("\nPre-order:")
+print("\nPreorder:")
 printPreorder(root)
 print()
-print("\nIn-order:")
+print("\nInorder:")
 printInorder(root)
 print()
-print("\nPost-order:")
+print("\nPostorder:")
 printPostorder(root)
+print()
